32ed8fd4499c3ede20fbfe9c1acb8d1975258d99
Hugo Herter <git@hugoherter.com>
2017-01-30 23:36:20 +0100

    Add redirect from / to app

--- hellodjango/hellodjango/urls.py
+++ hellodjango/hellodjango/urls.py
@@ -2,8 +2,11 @@
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(pattern_name='list_db_objects')),
     url(r'^hello-world/', include('helloworld.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- hellodjango/helloworld/urls.py
+++ hellodjango/helloworld/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^(?P<id_>\d+)$', views.say_hello, name='say_hello'),
+    url(r'^$', views.index, name='list_db_objects'),
+    url(r'^(?P<id_>\d+)/?$', views.say_hello, name='say_hello'),
 ]
