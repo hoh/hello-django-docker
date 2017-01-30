eea9743da6914305ebd0ffd8f9b4db265071f7c6
Hugo Herter <git@hugoherter.com>
2017-01-31 00:38:39 +0100

    Change DB path easy to mount

--- hellodjango/hellodjango/settings.py
+++ hellodjango/hellodjango/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+if os.path.isfile('/.dockerenv'):
+    # Running inside Docker, so we use a directory that can be mounted easily
+    DB_FILEPATH = '/data/db.sqlite3'
+else:
+    DB_FILEPATH = os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': DB_FILEPATH,
     }
 }
 
